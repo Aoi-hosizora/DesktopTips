17e7eca1ecbc4192adfc6637800d4c5b9f9cf119
AoiHosizora <a970335605@hotmail.com>
2019-04-13 23:22:52 +0800

    First Commit For Fnd

---
+++ Form1.Designer.vb
@@ -0,0 +1,202 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.ButtonRemoveItem = New DevComponents.DotNetBar.ButtonX()
+        Me.ButtonAddItem = New DevComponents.DotNetBar.ButtonX()
+        Me.StyleManager1 = New DevComponents.DotNetBar.StyleManager(Me.components)
+        Me.LabelFocus = New System.Windows.Forms.Label()
+        Me.ListView = New System.Windows.Forms.ListBox()
+        Me.ButtonCloseForm = New DevComponents.DotNetBar.ButtonX()
+        Me.ButtonChangeHeight = New DevComponents.DotNetBar.ButtonX()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.TimerShowForm = New System.Windows.Forms.Timer(Me.components)
+        Me.TimerEndForm = New System.Windows.Forms.Timer(Me.components)
+        Me.TimerMouseIn = New System.Windows.Forms.Timer(Me.components)
+        Me.TimerMouseOutWait = New System.Windows.Forms.Timer(Me.components)
+        Me.TimerMouseOut = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ButtonRemoveItem
+        '
+        Me.ButtonRemoveItem.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton
+        Me.ButtonRemoveItem.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ButtonRemoveItem.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground
+        Me.ButtonRemoveItem.Location = New System.Drawing.Point(176, 88)
+        Me.ButtonRemoveItem.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.ButtonRemoveItem.Name = "ButtonRemoveItem"
+        Me.ButtonRemoveItem.Shape = New DevComponents.DotNetBar.RoundRectangleShapeDescriptor()
+        Me.ButtonRemoveItem.Size = New System.Drawing.Size(24, 23)
+        Me.ButtonRemoveItem.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled
+        Me.ButtonRemoveItem.TabIndex = 1
+        Me.ButtonRemoveItem.Text = "－"
+        '
+        'ButtonAddItem
+        '
+        Me.ButtonAddItem.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton
+        Me.ButtonAddItem.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ButtonAddItem.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground
+        Me.ButtonAddItem.Location = New System.Drawing.Point(153, 88)
+        Me.ButtonAddItem.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.ButtonAddItem.Name = "ButtonAddItem"
+        Me.ButtonAddItem.Shape = New DevComponents.DotNetBar.RoundRectangleShapeDescriptor()
+        Me.ButtonAddItem.Size = New System.Drawing.Size(24, 23)
+        Me.ButtonAddItem.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled
+        Me.ButtonAddItem.TabIndex = 1
+        Me.ButtonAddItem.Text = "＋"
+        '
+        'StyleManager1
+        '
+        Me.StyleManager1.ManagerStyle = DevComponents.DotNetBar.eStyle.Office2007Blue
+        Me.StyleManager1.MetroColorParameters = New DevComponents.DotNetBar.Metro.ColorTables.MetroColorGeneratorParameters(System.Drawing.Color.White, System.Drawing.Color.FromArgb(CType(CType(43, Byte), Integer), CType(CType(87, Byte), Integer), CType(CType(154, Byte), Integer)))
+        '
+        'LabelFocus
+        '
+        Me.LabelFocus.AutoSize = True
+        Me.LabelFocus.Location = New System.Drawing.Point(92, 30)
+        Me.LabelFocus.Name = "LabelFocus"
+        Me.LabelFocus.Size = New System.Drawing.Size(48, 17)
+        Me.LabelFocus.TabIndex = 2
+        Me.LabelFocus.Text = "FOCUS"
+        Me.LabelFocus.Visible = False
+        '
+        'ListView
+        '
+        Me.ListView.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ListView.FormattingEnabled = True
+        Me.ListView.ItemHeight = 17
+        Me.ListView.Items.AddRange(New Object() {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"})
+        Me.ListView.Location = New System.Drawing.Point(0, 0)
+        Me.ListView.Name = "ListView"
+        Me.ListView.Size = New System.Drawing.Size(200, 89)
+        Me.ListView.TabIndex = 0
+        '
+        'ButtonCloseForm
+        '
+        Me.ButtonCloseForm.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton
+        Me.ButtonCloseForm.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.ButtonCloseForm.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground
+        Me.ButtonCloseForm.Location = New System.Drawing.Point(0, 88)
+        Me.ButtonCloseForm.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.ButtonCloseForm.Name = "ButtonCloseForm"
+        Me.ButtonCloseForm.Shape = New DevComponents.DotNetBar.RoundRectangleShapeDescriptor()
+        Me.ButtonCloseForm.Size = New System.Drawing.Size(24, 23)
+        Me.ButtonCloseForm.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled
+        Me.ButtonCloseForm.TabIndex = 1
+        Me.ButtonCloseForm.Text = "×"
+        '
+        'ButtonChangeHeight
+        '
+        Me.ButtonChangeHeight.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton
+        Me.ButtonChangeHeight.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.ButtonChangeHeight.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground
+        Me.ButtonChangeHeight.Location = New System.Drawing.Point(23, 88)
+        Me.ButtonChangeHeight.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.ButtonChangeHeight.Name = "ButtonChangeHeight"
+        Me.ButtonChangeHeight.Shape = New DevComponents.DotNetBar.RoundRectangleShapeDescriptor()
+        Me.ButtonChangeHeight.Size = New System.Drawing.Size(24, 23)
+        Me.ButtonChangeHeight.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled
+        Me.ButtonChangeHeight.TabIndex = 1
+        Me.ButtonChangeHeight.Text = "≡"
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.NumericUpDown1.Location = New System.Drawing.Point(46, 88)
+        Me.NumericUpDown1.Minimum = New Decimal(New Integer() {5, 0, 0, 0})
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(45, 23)
+        Me.NumericUpDown1.TabIndex = 3
+        Me.NumericUpDown1.Value = New Decimal(New Integer() {8, 0, 0, 0})
+        Me.NumericUpDown1.Visible = False
+        '
+        'TimerShowForm
+        '
+        Me.TimerShowForm.Enabled = True
+        Me.TimerShowForm.Interval = 1
+        '
+        'TimerEndForm
+        '
+        Me.TimerEndForm.Interval = 1
+        '
+        'TimerMouseIn
+        '
+        Me.TimerMouseIn.Interval = 10
+        '
+        'TimerMouseOutWait
+        '
+        Me.TimerMouseOutWait.Interval = 10
+        '
+        'TimerMouseOut
+        '
+        Me.TimerMouseOut.Interval = 10
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 17.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.DarkRed
+        Me.ClientSize = New System.Drawing.Size(200, 112)
+        Me.Controls.Add(Me.ListView)
+        Me.Controls.Add(Me.ButtonChangeHeight)
+        Me.Controls.Add(Me.ButtonCloseForm)
+        Me.Controls.Add(Me.ButtonAddItem)
+        Me.Controls.Add(Me.ButtonRemoveItem)
+        Me.Controls.Add(Me.LabelFocus)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Font = New System.Drawing.Font("Microsoft YaHei UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.MinimumSize = New System.Drawing.Size(150, 112)
+        Me.Name = "Form1"
+        Me.Opacity = 0.0R
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        Me.Text = "DesktopTips"
+        Me.TransparencyKey = System.Drawing.Color.DarkRed
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ButtonRemoveItem As DevComponents.DotNetBar.ButtonX
+    Friend WithEvents ButtonAddItem As DevComponents.DotNetBar.ButtonX
+    Friend WithEvents StyleManager1 As DevComponents.DotNetBar.StyleManager
+    Friend WithEvents LabelFocus As System.Windows.Forms.Label
+    Friend WithEvents ListView As System.Windows.Forms.ListBox
+    Friend WithEvents ButtonCloseForm As DevComponents.DotNetBar.ButtonX
+    Friend WithEvents ButtonChangeHeight As DevComponents.DotNetBar.ButtonX
+    Friend WithEvents NumericUpDown1 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents TimerShowForm As System.Windows.Forms.Timer
+    Friend WithEvents TimerEndForm As System.Windows.Forms.Timer
+    Friend WithEvents TimerMouseIn As System.Windows.Forms.Timer
+    Friend WithEvents TimerMouseOutWait As System.Windows.Forms.Timer
+    Friend WithEvents TimerMouseOut As System.Windows.Forms.Timer
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,203 @@
+﻿Public Class Form1
+
+    Declare Sub mouse_event Lib "user32" (ByVal dwFlags As Integer, ByVal dx As Integer, ByVal dy As Integer, ByVal cButtons As Integer, ByVal dwExtraInfo As Integer)
+    Private Declare Function SetCursorPos Lib "user32" (ByVal x As Long, ByVal y As Long) As Long
+    ' Private Declare Sub mouse_event Lib "user32" (ByVal dwFlags As Long, ByVal dx As Long, ByVal dy As Long, ByVal cButtons As Long, ByVal dwExtraInfo As Long)
+
+    Enum MouseEvent
+        MOUSEEVENTF_LEFTDOWN = &H2      '左键按下
+        MOUSEEVENTF_LEFTUP = &H4        '左键释放
+        MOUSEEVENTF_MIDDLEDOWN = &H20   '中键按下
+        MOUSEEVENTF_MIDDLEUP = &H40     '中键释放
+        MOUSEEVENTF_RIGHTDOWN = &H8     '右键按下
+        MOUSEEVENTF_RIGHTUP = &H10      '右键释放
+        MOUSEEVENTF_MOVE = &H1          '指针移动
+    End Enum
+
+    Private PushDownMouseInScreen As Point
+    Private PushDownWindowPos As Point
+    Private PushDownWindowSize As Point
+    Private IsMouseDown As Boolean
+    Private IsChangeSize As Boolean
+
+    Private Sub ListView_MouseDown(sender As Object, e As System.Windows.Forms.MouseEventArgs) Handles ListView.MouseDown
+        If e.Button = Windows.Forms.MouseButtons.Left Then
+            IsMouseDown = True
+        End If
+        PushDownMouseInScreen = Cursor.Position
+        PushDownWindowPos = New Point(Me.Left, Me.Top)
+        PushDownWindowSize = New Point(Me.Width, Me.Height)
+    End Sub
+
+    Private Sub ListView_MouseMove(sender As Object, e As System.Windows.Forms.MouseEventArgs) Handles ListView.MouseMove
+
+        If e.X > Me.Width - 10 Or IsChangeSize = True Then
+            Me.Cursor = Cursors.SizeWE
+            If IsMouseDown Then
+                IsChangeSize = True
+                Me.Width = PushDownWindowSize.X + Cursor.Position.X - PushDownMouseInScreen.X
+            End If
+        Else
+            Me.Cursor = Cursors.Default
+            If IsMouseDown And Not IsChangeSize Then
+                Me.Top = PushDownWindowPos.Y + Cursor.Position.Y - PushDownMouseInScreen.Y
+                Me.Left = PushDownWindowPos.X + Cursor.Position.X - PushDownMouseInScreen.X
+            End If
+        End If
+    End Sub
+
+    Private Sub ListView_MouseUp(sender As Object, e As System.Windows.Forms.MouseEventArgs) Handles ListView.MouseUp
+        IsMouseDown = False
+        IsChangeSize = False
+    End Sub
+
+    Private Sub ButtonFocus_Handle(sender As Object, e As System.Windows.Forms.MouseEventArgs) Handles ButtonAddItem.MouseDown, ButtonRemoveItem.MouseDown, ButtonCloseForm.MouseDown, ButtonChangeHeight.MouseDown
+        LabelFocus.Select()
+    End Sub
+
+    ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+    ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+    ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+    Dim SelectItem As Integer
+    Private Sub ButtonAddItem_Click(sender As System.Object, e As System.EventArgs) Handles ButtonAddItem.Click
+        Dim msg As String = InputBox("新的提醒标签：", "添加")
+        If msg <> "" Then
+            ListView.Items.Add(msg.Trim())
+        End If
+    End Sub
+
+    Private Sub ButtonRemoveItem_Click(sender As System.Object, e As System.EventArgs) Handles ButtonRemoveItem.Click
+        If (ListView.SelectedItem IsNot Nothing) Then
+            Dim ok As Integer = MsgBox("确定删除提醒标签 """ & ListView.SelectedItem & """ 吗？", MsgBoxStyle.OkCancel, "删除")
+            If (ok = vbOK) Then
+                ListView.Items.RemoveAt(SelectItem)
+            End If
+        End If
+    End Sub
+
+    Private Sub ListView_DoubleClick(sender As Object, e As System.EventArgs) Handles ListView.DoubleClick
+        If (ListView.SelectedItem IsNot Nothing) Then
+            Dim newstr As String = InputBox("修改提醒标签 """ & ListView.SelectedItem & """ 为：", "修改", ListView.SelectedItem)
+            If newstr <> "" Then
+                ListView.Items(ListView.SelectedIndex) = newstr.Trim()
+            End If
+        End If
+    End Sub
+
+    Private Sub ListView_SelectedIndexChanged(sender As System.Object, e As System.EventArgs) Handles ListView.SelectedIndexChanged
+        ButtonRemoveItem.Enabled = ListView.SelectedItem IsNot Nothing
+    End Sub
+
+    Private Sub ListView_MouseClick(sender As Object, e As System.Windows.Forms.MouseEventArgs) Handles ListView.MouseClick
+        If ListView.IndexFromPoint(e.X, e.Y) = -1 Then
+            ListView.ClearSelected()
+        End If
+    End Sub
+
+    Private Const AppName As String = "DesktopTips"
+    Private Const Section As String = "FormPosSize"
+
+    Private Sub Form1_Deactivate(sender As Object, e As System.EventArgs) Handles Me.Deactivate
+        SelectItem = ListView.SelectedIndex
+        ListView.ClearSelected()
+    End Sub
+
+    Private Sub Form1_FormClosed(sender As Object, e As System.Windows.Forms.FormClosedEventArgs) Handles Me.FormClosed
+        SaveSetting(AppName, Section, "Top", Me.Top)
+        SaveSetting(AppName, Section, "Left", Me.Left)
+        SaveSetting(AppName, Section, "Height", Me.Height)
+        SaveSetting(AppName, Section, "Width", Me.Width)
+    End Sub
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        Me.Top = GetSetting(AppName, Section, "Top", 20)
+        Me.Left = GetSetting(AppName, Section, "Left", 20)
+        Me.Height = GetSetting(AppName, Section, "Height", 163)
+        Me.Width = GetSetting(AppName, Section, "Width", 200)
+        NumericUpDown1.Value = (Me.Height - 27) \ 17
+        ButtonRemoveItem.Enabled = False
+    End Sub
+
+    Private Sub ButtonCloseForm_Click(sender As System.Object, e As System.EventArgs) Handles ButtonCloseForm.Click
+        TimerEndForm.Enabled = True
+    End Sub
+
+    Private Sub NumericUpDown1_ValueChanged(sender As System.Object, e As System.EventArgs) Handles NumericUpDown1.ValueChanged
+        Dim MoveY As Integer
+        If Me.Height < NumericUpDown1.Value * 17 + 27 Then
+            MoveY = 10
+        ElseIf Me.Height > NumericUpDown1.Value * 17 + 27 Then
+            MoveY = -10
+        End If
+        Me.Height = NumericUpDown1.Value * 17 + 27
+        mouse_event(MouseEvent.MOUSEEVENTF_MOVE, 0, MoveY, 0, 0)
+    End Sub
+
+    Private Sub ButtonChangeHeight_Click(sender As System.Object, e As System.EventArgs) Handles ButtonChangeHeight.Click
+        ButtonChangeHeight.Checked = Not ButtonChangeHeight.Checked
+        NumericUpDown1.Visible = ButtonChangeHeight.Checked
+    End Sub
+
+    Private Sub TimerShowForm_Tick(sender As System.Object, e As System.EventArgs) Handles TimerShowForm.Tick
+        Me.Opacity += 0.08
+        Me.Top += 1
+        If Me.Opacity >= 0.6 Then
+            TimerShowForm.Enabled = False
+        End If
+    End Sub
+
+    Private Sub TimerEndForm_Tick(sender As System.Object, e As System.EventArgs) Handles TimerEndForm.Tick
+        Me.Opacity -= 0.08
+        Me.Top -= 1
+        If Me.Opacity <= 0 Then
+            TimerEndForm.Enabled = False
+            Me.Close()
+        End If
+        If TimerShowForm.Enabled = True Then
+            TimerShowForm.Enabled = False
+        End If
+    End Sub
+
+    Private Sub TimerMouseIn_Tick(sender As System.Object, e As System.EventArgs) Handles TimerMouseIn.Tick
+        Me.Opacity += 0.02
+        If Me.Opacity >= 1 Then
+            TimerMouseIn.Enabled = False
+        End If
+        If TimerMouseOut.Enabled = True Then
+            TimerMouseOut.Enabled = False
+        End If
+    End Sub
+
+    'Private Sub TimerMouseOutWait_Tick(sender As System.Object, e As System.EventArgs) Handles TimerMouseOutWait.Tick
+    '    If TimerMouseIn.Enabled = True Then
+    '        TimerMouseIn.Enabled = False
+    '    End If
+    '    MouseLeaveCnt += 1
+    '    If MouseLeaveCnt > 90 Then
+    '        TimerMouseOut.Enabled = True
+    '        TimerMouseOutWait.Enabled = False
+    '    End If
+    'End Sub
+
+    Private Sub TimerMouseOut_Tick(sender As System.Object, e As System.EventArgs) Handles TimerMouseOut.Tick
+        Me.Opacity -= 0.02
+        If Me.Opacity <= 0.6 Then
+            TimerMouseOut.Enabled = False
+            Me.Opacity = 0.6
+        End If
+        If TimerMouseIn.Enabled = True Then
+            TimerMouseIn.Enabled = False
+        End If
+    End Sub
+
+    Private Sub ListView_MouseEnter(sender As Object, e As System.EventArgs) Handles ListView.MouseEnter
+        TimerMouseIn.Enabled = True
+    End Sub
+
+    Private MouseLeaveCnt As Integer
+    Private Sub ListView_MouseLeave(sender As Object, e As System.EventArgs) Handles ListView.MouseLeave
+        MouseLeaveCnt = 0
+        TimerMouseOut.Enabled = True
+    End Sub
+End Class

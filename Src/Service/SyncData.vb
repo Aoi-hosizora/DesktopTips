9ace96a358b60ea90a49477c8390149d29320a42
AoiHosizora <a970335605@hotmail.com>
2019-10-18 22:08:25 +0800

    Add Some Fix & Implementation In Sync (Include Bug)

--- Src/Service/SyncData.vb
+++ Src/Service/SyncData.vb
@@ -3,70 +3,133 @@ Imports System.Net.Sockets
 Imports System.Net
 Imports System.Text
 Imports Newtonsoft.Json
+Imports System.IO
 
 Public Class SyncData
+
+    Public Delegate Sub SendTabsCb(ByVal ok As Exception)
+
+
+    Public Shared sendClientSocket As Socket = Nothing
 
     ''' <summary>
     ''' 同步远程 (本地 C -> 安卓 S)
     ''' 远程监听地址 -> 确定远程地址 -> 本地发送数据 -> 等待 ACK
     ''' </summary>
     <MethodImpl(MethodImplOptions.Synchronized)>
-    Public Shared Function SendTabs(ByVal Ip As String, ByVal Port As Integer) As Boolean
+    Public Shared Sub SendTabs(ByVal Ip As String, ByVal Port As Integer, ByVal cb As SendTabsCb)
+        Dim ret As New Exception
+
+        ' ソケットが接続されていないか、sendto 呼び出しを使ってデータグラム ソケットで送信するときにアドレスが指定されていないため、データの送受信を要求することは禁じられています。
+        If sendClientSocket IsNot Nothing Then
+            sendClientSocket.Close()
+        End If
+        sendClientSocket = Nothing
         Try
-            Dim socket As New Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp)
-            socket.Bind(New IPEndPoint(IPAddress.Parse(Ip), Port))
+            sendClientSocket = New Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp)
+            If sendClientSocket Is Nothing Then
+                Throw New Exception()
+            End If
+            sendClientSocket.Connect(New IPEndPoint(IPAddress.Parse(Ip), Port))
 
             Dim json As String = JsonConvert.SerializeObject(GlobalModel.Tabs, Formatting.Indented)
+            Console.WriteLine(json.Length)
             If Not String.IsNullOrWhiteSpace(json) Then
-                socket.Send(Encoding.UTF8.GetBytes(json.ToArray()))
-                Return True
+                sendClientSocket.Send(Encoding.UTF8.GetBytes(json.ToArray()))
+                sendClientSocket.Close()
+                ret = Nothing
             End If
         Catch ex As Exception
             Console.WriteLine(ex.Message)
+            ret = ex
         End Try
-        Return False
-    End Function
+
+        If sendClientSocket IsNot Nothing Then
+            sendClientSocket.Close()
+        End If
+        cb(ret)
+    End Sub
+
+    Public Shared rcvServerSocket As TcpListener = Nothing
+
+    Public Delegate Sub ReceiveTabsCb(ByVal ret As String, ByVal ok As Exception)
 
     ''' <summary>
     ''' 同步本地 (安卓 C -> 本地 S)
     ''' 确定端口 -> 监听本地地址 -> 电脑端发送 -> 本地接受处理
     ''' </summary>
-    ''' <returns>Error -> ""</returns>
     <MethodImpl(MethodImplOptions.Synchronized)>
-    Public Shared Function ReceiveTabs(ByVal Port As Integer) As String
-        Dim server As New TcpListener(New IPEndPoint(IPAddress.Parse("0.0.0.0"), Port))
+    Public Shared Sub ReceiveTabs(ByVal Port As Integer, ByVal cb As ReceiveTabsCb)
+
+        ' 待っている状態からスレッドが中断されました。
+
+        Dim ret As String = ""
+        Dim ok As New Exception
+
+        rcvServerSocket = New TcpListener(New IPEndPoint(IPAddress.Parse("0.0.0.0"), Port))
+
         Try
-            server.Start()
+            rcvServerSocket.Start()
         Catch ex As Exception
             Console.WriteLine(ex.Message)
-            Return ""
+            ok = ex
+            GoTo eof
         End Try
+
         While True
             Dim client As TcpClient
             Try
-                client = server.AcceptTcpClient()
+                client = rcvServerSocket.AcceptTcpClient()
             Catch ex As Exception
                 Console.WriteLine(ex.Message)
-                Return ""
+                ok = ex
+                GoTo eof
             End Try
 
+            ' 算術演算の結果オーバーフローが発生しました。
             Try
                 Dim stream As NetworkStream = client.GetStream()
-                Dim srcBufList As New List(Of Byte)
-                Dim bt As Byte = stream.ReadByte()
-                While bt <> -1
-                    srcBufList.Add(bt)
-                    bt = stream.ReadByte()
-                End While
+                Dim data As String
+
+                ' 读取流
+                SyncLock stream
+                    Dim reader As New StreamReader(stream)
+                    data = reader.ReadToEnd()
+                End SyncLock
+
+                ok = Nothing
+                ret = data
+
                 stream.Close()
                 client.Close()
-                server.Stop()
-                Return Encoding.UTF8.GetString(srcBufList.ToArray())
+                GoTo eof
             Catch ex As Exception
                 Console.Write(ex.Message)
+                ok = ex
+                GoTo eof
+            Finally
+                If rcvServerSocket IsNot Nothing Then
+                    rcvServerSocket.Stop()
+                End If
             End Try
         End While
-        Return ""
+
+eof:
+        If rcvServerSocket IsNot Nothing Then
+            rcvServerSocket.Stop()
+        End If
+        cb(ret, ok)
+    End Sub
+
+    Public Shared Function GetLanIP() As String
+        Dim ip As String = String.Empty
+        For Each ipa As IPAddress In Dns.GetHostAddresses(Dns.GetHostName())
+            If ipa.AddressFamily = AddressFamily.InterNetwork Then
+                ip = ipa.ToString()
+            End If
+        Next
+        Return ip
     End Function
+
 
 End Class
--- Src/UI/MainForm.vb
+++ Src/UI/MainForm.vb
@@ -1,7 +1,11 @@
 ﻿Imports System.IO
 Imports System.Text
+Imports System.Threading
+
 Imports SU = DesktopTips.GlobalModel
 Imports DD = DevComponents.DotNetBar
+Imports QRCoder
+Imports System.Text.RegularExpressions
 
 Public Class MainForm
 
@@ -334,9 +338,9 @@ Public Class MainForm
                 New DD.BaseItem() {Me.ListPopupMenuLabelSelItem, Me.ListPopupMenuLabelSelItemText, Me.ListPopupMenuLabelItemList, _
                                    Me.ListPopupMenuItemContainer, _
                                    Me.ListPopupMenuMoveTop, Me.ListPopupMenuMoveBottom, Me.ListPopupMenuViewHighLight, Me.ListPopupMenuFind, Me.ListPopupMenuMove, _
-                                   Me.ListPopupMenuLabelItemFile, Me.ListPopupMenuOpenDir, Me.ListPopupMenuViewFile, Me.ListPopupMenuOpenBrowser, _
+                                   Me.ListPopupMenuLabelItemFile, Me.ListPopupMenuOpenDir, Me.ListPopupMenuViewFile, Me.ListPopupMenuOpenBrowser, Me.ListPopupMenuSyncData, _
                                    Me.ListPopupMenuLabelItemWindow, Me.ListPopupMenuWinSetting, Me.ListPopupMenuExit})
-            
+
             Me.ListPopupMenuAddItem.BeginGroup = True
             Me.ListPopupMenuMoveUp.BeginGroup = True
             Me.ListPopupMenuMoveTop.BeginGroup = True
@@ -363,7 +367,7 @@ Public Class MainForm
                                    Me.ListPopupMenuMoveUp, Me.ListPopupMenuMoveDown, Me.ListPopupMenuMoveTop, Me.ListPopupMenuMoveBottom, _
                                    Me.ListPopupMenuCopy, Me.ListPopupMenuSelectAll, Me.ListPopupMenuHighLight, Me.ListPopupMenuViewHighLight, _
                                    Me.ListPopupMenuFind, Me.ListPopupMenuMove, _
-                                   Me.ListPopupMenuLabelItemFile, Me.ListPopupMenuOpenDir, Me.ListPopupMenuViewFile, Me.ListPopupMenuOpenBrowser, _
+                                   Me.ListPopupMenuLabelItemFile, Me.ListPopupMenuOpenDir, Me.ListPopupMenuViewFile, Me.ListPopupMenuOpenBrowser, Me.ListPopupMenuSyncData, _
                                    Me.ListPopupMenuLabelItemWindow, Me.ListPopupMenuWinSetting, Me.ListPopupMenuExit})
 
         End If
@@ -1373,17 +1377,104 @@ Public Class MainForm
 
 #Region "数据同步"
 
+    Private Shared QR_CODE_MAGIC As String = "DESKTOP_TIPS_ANDROID://"
+
     ''' <summary>
-    ''' 同步到移动端
+    ''' 同步到移动端 (本地 C -> 安卓 S)
     ''' </summary>
     Private Sub ListPopupMenuSyncDataTo_Click(sender As System.Object, e As System.EventArgs) Handles ListPopupMenuSyncDataTo.Click
+        Dim input As String = InputBox("请输入移动端的地址：", "同步到移动端", "127.0.0.1:8776")
+
+        If String.IsNullOrWhiteSpace(input) Then Return ' 空内容
+        Dim sp As String() = input.Split(New Char() {":"})
+
+        Dim ipRe As New Regex("^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$")
+        Dim portRe As New Regex("^([0-9]|[1-9]\d{1,3}|[1-5]\d{4}|6[0-4]\d{4}|65[0-4]\d{2}|655[0-2]\d|6553[0-5])$")
+
+        Dim ip As String, port As Integer
+        If sp.Length <> 2 OrElse Not ipRe.IsMatch(sp(0)) OrElse Not portRe.IsMatch(sp(1)) OrElse Not Integer.TryParse(sp(1), port) Then
+            MessageBox.Show("所输入的地址格式不正确。", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return
+        End If
+        ip = sp(0)
+
+        Dim thread As New Thread(New ThreadStart(Sub() _
+            SyncData.SendTabs(Ip:=ip, Port:=port, cb:= _
+                Sub(ok As Exception)
+                    If ok Is Nothing Then
+                        Me.Invoke(New Action(Sub() _
+                            MessageBox.Show("数据发送成功，请在移动端确认。", "同步到移动端", MessageBoxButtons.OK, MessageBoxIcon.Information)))
+                    Else
+                        Me.Invoke(New Action(Sub() _
+                            MessageBox.Show("数据发送错误。" + Chr(10) + ok.Message, "错误", MessageBoxButtons.OK, MessageBoxIcon.Error)))
+                    End If
+                End Sub)))
+        thread.Start()
 
     End Sub
 
     ''' <summary>
-    ''' 从移动端同步
+    ''' 从移动端同步 (安卓 C -> 本地 S)
     ''' </summary>
     Private Sub ListPopupMenuSyncDataFrom_Click(sender As System.Object, e As System.EventArgs) Handles ListPopupMenuSyncDataFrom.Click
+        Dim ip As String = SyncData.GetLanIP()
+        If String.IsNullOrWhiteSpace(ip) Then                                                                                               ' 地址错误
+            MessageBox.Show("本机获取局域网内地址错误。", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return
+        End If
+
+        Dim input As String = InputBox("请输入本地监听的端口：", "从移动端同步", "8776")
+        Dim port As Integer
+        If String.IsNullOrWhiteSpace(input) Then Return ' 空内容
+        If Not Integer.TryParse(input, port) OrElse Not (port >= 1 And port <= 65535) Then                                                  ' 端口错误
+            MessageBox.Show("所输入的端口格式不正确，应为在 [1, 65535] 内的纯数字。", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return
+        End If
+
+        Dim thread As Thread = Nothing
+
+        Dim qrData As String = QR_CODE_MAGIC + ip + ":" + port.ToString
+        Dim qrGenerator As New QRCodeGenerator
+        Dim qrCodeData As QRCodeData = qrGenerator.CreateQrCode(qrData, QRCodeGenerator.ECCLevel.Q)
+        Dim qrCode As New QRCode(qrCodeData)
+        Dim qrCodeImg As Bitmap = qrCode.GetGraphic(7)
+
+        Dim qrCodeForm As New Form With {.Text = "连接二维码 (" & ip & ":" & port & ")", .Name = "qrCodeForm", .FormBorderStyle = Windows.Forms.FormBorderStyle.FixedDialog, _
+                                         .MaximizeBox = False, .MinimizeBox = False, .ShowInTaskbar = False, _
+                                         .StartPosition = FormStartPosition.CenterScreen, .Size = qrCodeImg.Size}
+
+        Dim pictureBox As New PictureBox With {.Name = "pictureBox", .SizeMode = PictureBoxSizeMode.Zoom, .Image = qrCodeImg}
+
+        qrCodeForm.Controls.Add(pictureBox)
+        pictureBox.Dock = DockStyle.Fill
+        AddHandler qrCodeForm.FormClosed, _
+            Sub()
+                'If SyncData.rcvServerSocket IsNot Nothing Then
+                '    SyncData.rcvServerSocket.Stop()
+                'End If
+                'If thread IsNot Nothing Then
+                '    thread.Abort()
+                'End If
+            End Sub
+
+        qrCodeForm.Show(Me)
+
+        ' 受け付け可能になっていません。このメソッドを呼び出す前に、Start() メソッドを呼び出してください。
+        thread = New Thread(New ThreadStart(Sub() _
+            SyncData.ReceiveTabs(Port:=port, cb:= _
+                Sub(ret As String, ok As Exception)
+                    Me.Invoke(New Action( _
+                        Sub()
+                            qrCodeForm.Close()
+                            If ok Is Nothing Then
+                                MessageBox.Show(ret, "")
+                            Else
+                                MessageBox.Show("数据接收错误。" + Chr(10) + ok.Message, "错误", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                            End If
+                        End Sub))
+                End Sub)))
+
+        thread.Start()
 
     End Sub
 
